a1cd6641ec537f47fc87d9dce7e7dfc1239f97ac
Filippo <filippo.schiavio@gmail.com>
2014-12-09 13:58:07 +0100

    Stavolta è un hard link, vediamo se funziona!

--- tailrec.py
+++ tailrec.py
@@ -1,1 +1,27 @@
-../../mie-prove/tail-rec/tailrec.py
+class tailrec:
+    def __init__(self,func):
+        self.func = func
+        self.recursivecall = False
+        self.lastcall = False
+    def __call__(self,*args,**kargs):
+        print('Intercept call {0}{1}'.format(self.func, (args,kargs)))
+        if self.recursivecall:
+            self.recursivecall = False
+            self.lastcall = False
+            return (args, kargs)
+        else:
+            newargs, newkargs = args, kargs
+            while True:
+                self.recursivecall = True
+                self.lastcall = True
+                x = self.func(*newargs, **newkargs)
+                if self.lastcall: return x
+                newargs, newkargs = x[0], x[1]
+
+
+@tailrec
+def tailrecsum(x,y):
+    ''' Efficient sum algorithm :) '''
+    return y if x==0 else tailrecsum(x-1,y+1) 
+    
+print(tailrecsum(3,2))
